2fcb0d8ed9c0cb42eca194e1e490812b10bf8504
Sadid Rafan <sadidrafan094@gmail.com>
2025-06-10 23:44:19 +0600

    project setup

---
+++ src/main/java/com/contactly/ContactlyApplication.java
@@ -0,0 +1,13 @@
+package com.contactly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/contactly/ContactlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contactly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
