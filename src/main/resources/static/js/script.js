c4d029ac30afd3f1bba47a79fd81ea2107c80d42
Sadid Rafan <sadidrafan094@gmail.com>
2025-06-14 04:19:34 +0600

    change theme functionality

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,56 @@
+console.log("Script loaded ");
+
+let currentTheme = getTheme();
+
+document.addEventListener("DOMContentLoaded", () => {
+  changeTheme();
+});
+
+// initially set the theme
+
+// todo
+function changeTheme() {
+  // set to webpage
+  changePageTheme(currentTheme, currentTheme);
+
+  const changeThemeButton = document.querySelector("#theme_change_button");
+  const oldTheme = currentTheme;
+  changeThemeButton.addEventListener("click", (event) => {
+    console.log("theme change Button clicked");
+
+    if (currentTheme === "dark") {
+      currentTheme = "light";
+    } else {
+      currentTheme = "dark";
+    }
+
+    changePageTheme(currentTheme, oldTheme);
+  });
+}
+
+function setTheme(theme) {
+  localStorage.setItem("theme", theme);
+}
+
+function getTheme() {
+  let theme = localStorage.getItem("theme");
+
+  if (theme) return theme;
+  else return "light";
+}
+
+function changePageTheme(theme, oldTheme) {
+  // update in localstorage
+
+  setTheme(currentTheme);
+  // remove  current theme
+  document.querySelector("html").classList.remove(oldTheme);
+
+  // add new theme
+  document.querySelector("html").classList.add(theme);
+
+  // change the text
+  document
+    .querySelector("#theme_change_button")
+    .querySelector("span").textContent = theme == "light" ? "Dark" : "Light";
+}
