86108051313ac6a8f955feae957044ae89f20f89
Sadid Rafan <sadidrafan094@gmail.com>
2025-06-20 22:40:19 +0600

    error validation add

--- src/main/java/com/contactly/controllers/PageController.java
+++ src/main/java/com/contactly/controllers/PageController.java
@@ -3,6 +3,7 @@ package com.contactly.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -13,6 +14,7 @@ import com.contactly.helpers.MessageType;
 import com.contactly.services.UserService;
 
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -83,12 +85,16 @@ public class PageController {
     // processing the register form data 
 
     @RequestMapping(value = "/do-register" , method = RequestMethod.POST)
-    public String processRegister(@ModelAttribute UserForm userForm , HttpSession session){
+    public String processRegister(@Valid @ModelAttribute UserForm userForm ,BindingResult rBindingResult, HttpSession session){
 
         // fetch form data
 
         // validate form data
-        
+        if(rBindingResult.hasErrors()){
+             return "register"; // return to register page with error message
+        }
+
+
         // save to database
         
         // userSerivce
--- src/main/java/com/contactly/forms/UserForm.java
+++ src/main/java/com/contactly/forms/UserForm.java
@@ -1,5 +1,8 @@
 package com.contactly.forms;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,10 +18,21 @@ import lombok.ToString;
 @ToString 
 public class UserForm {
 
+    @NotBlank(message = "Name is required")
+    @Size(min=2, message = "Min 3 character is required")
     private String name;
+
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is required")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min=6 , message = "Min 6 character is required")
     private String password;
+
+    @NotBlank(message = "about is required")
     private String about;
+    @Size(min=8 , max=12 , message = "Phone number must be between 8 and 12 characters")
     private String phoneNumber;
 
 
