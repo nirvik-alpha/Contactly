c4b995be882055af48daf7b9bde6ac04bbdfc6eb
Sadid Rafan <sadidrafan094@gmail.com>
2025-08-02 20:09:34 +0600

    github authentication done

--- src/main/java/com/contactly/config/OAuthAuthenticationSuccessHandler.java
+++ src/main/java/com/contactly/config/OAuthAuthenticationSuccessHandler.java
@@ -6,7 +6,9 @@ import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -15,6 +17,7 @@ import org.springframework.stereotype.Component;
 import com.contactly.entites.Providers;
 import com.contactly.entites.User;
 import com.contactly.helpers.AppConstants;
+import com.contactly.repositories.UserRepo;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -26,13 +29,81 @@ public class OAuthAuthenticationSuccessHandler implements AuthenticationSuccessH
 
     Logger logger= LoggerFactory.getLogger(OAuthAuthenticationSuccessHandler.class);
     
+    @Autowired
+    private UserRepo userRepo;
     
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
             Authentication authentication) throws IOException, ServletException {
      
                logger.info("OAuthAuthenticationSuccessHandler");
-                
+        
+                // identify the provider
+
+        var oauth2AuthenicationToken = (OAuth2AuthenticationToken) authentication;
+
+        String authorizedClientRegistrationId = oauth2AuthenicationToken.getAuthorizedClientRegistrationId();
+
+
+        logger.info(authorizedClientRegistrationId);
+
+        var oauthUser = (DefaultOAuth2User) authentication.getPrincipal();
+
+        oauthUser.getAttributes().forEach((key, value) -> {
+            logger.info(key + " : " + value);
+        });
+
+        User user = new User();
+        user.setUserId(UUID.randomUUID().toString());
+        user.setRoleList(List.of(AppConstants.ROLE_USER));
+        user.setEmailVerified(true);
+        user.setEnabled(true);
+        user.setPassword("dummy");
+
+
+        if (authorizedClientRegistrationId.equalsIgnoreCase("google")) {
+
+            // google
+            // google attributes
+
+            user.setEmail(oauthUser.getAttribute("email").toString());
+            user.setProfilePic(oauthUser.getAttribute("picture").toString());
+            user.setName(oauthUser.getAttribute("name").toString());
+            user.setProviderUserId(oauthUser.getName());
+            user.setProvider(Providers.GOOGLE);
+            user.setAbout("This account is created using google.");
+
+        } else if (authorizedClientRegistrationId.equalsIgnoreCase("github")) {
+
+            // github
+            // github attributes
+            String email = oauthUser.getAttribute("email") != null ? oauthUser.getAttribute("email").toString()
+                    : oauthUser.getAttribute("login").toString() + "@gmail.com";
+            String picture = oauthUser.getAttribute("avatar_url").toString();
+            String name = oauthUser.getAttribute("login").toString();
+            String providerUserId = oauthUser.getName();
+
+            user.setEmail(email);
+            user.setProfilePic(picture);
+            user.setName(name);
+            user.setProviderUserId(providerUserId);
+            user.setProvider(Providers.GITHUB);
+
+            user.setAbout("This account is created using github");
+        }
+
+        else if (authorizedClientRegistrationId.equalsIgnoreCase("linkedin")) {
+
+        }
+
+        else {
+            logger.info("OAuthAuthenicationSuccessHandler: Unknown provider");
+        }
+
+
+
+               /* 
+
                DefaultOAuth2User user =(DefaultOAuth2User)authentication.getPrincipal();
                // data save to database
 
@@ -59,6 +130,24 @@ public class OAuthAuthenticationSuccessHandler implements AuthenticationSuccessH
                          user1.setProviderUserId(user.getName());
                          user1.setRoleList(List.of(AppConstants.ROLE_USER));
                          user1.setAbout("This account is created using google..");
+
+                         User user2 = userRepo.findByEmail(email).orElse(null);
+
+                         if (user2 == null) 
+                         {
+                             userRepo.save(user1);
+                            logger.info("User saved:" + email);
+                         }
+
+                */
+
+                User user2 = userRepo.findByEmail(user.getEmail()).orElse(null);
+                 if (user2 == null) 
+                 {
+                   userRepo.save(user);
+                System.out.println("user saved:" + user.getEmail());
+                  }
+
 
 
                new DefaultRedirectStrategy().sendRedirect(request, response, "/user/dashboard");
--- src/main/java/com/contactly/controllers/UserController.java
+++ src/main/java/com/contactly/controllers/UserController.java
@@ -1,15 +1,22 @@
 package com.contactly.controllers;
 
+import java.security.Principal;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.contactly.helpers.Helper;
 
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
+     private Logger logger = LoggerFactory.getLogger(UserController.class);
     // user dashboard
 
     @RequestMapping(value="/dashboard")
@@ -19,7 +26,11 @@ public class UserController {
     }
     
  @RequestMapping(value="/profile")
-    public String userProfile() {
+    public String userProfile(Authentication  authentication) {
+       
+       String name = Helper.getEmailOfLoggedInUser(authentication);
+        
+        logger.info("User profile accessed by: " + name);
         System.out.println("User profile");
         return "user/profile";
     }
